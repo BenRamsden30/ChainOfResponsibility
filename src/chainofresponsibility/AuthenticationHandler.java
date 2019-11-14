d0a5cae9a5c1e11eebf5fddb186662b24f50b3f5
BEN RAMSDEN <V8036651@live.tees.ac.uk>
2019-11-14 13:58:02 +0000

    Working on authenticate

--- src/chainofresponsibility/AuthenticationHandler.java
+++ src/chainofresponsibility/AuthenticationHandler.java
@@ -26,147 +26,25 @@ public class AuthenticationHandler
         ClearanceHandler test = new ClearanceHandler();
         System.out.println("What is your clearance level?");
         clearance = scanner.nextInt();
-        request = "";
-        if (test.ClearanceCheck(clearance) == true)
-        {
-        while (request.equals("EXIT") == false)
-        {
-            while (approved == false)
-            {
-                System.out.println("What level of request would you like to make?");
-                request = scanner.nextLine().toUpperCase();
-                switch (request) {
-                    case "GENERAL STAFF":
-                        {
-                            GeneralStaffHandler testGeneralStaff = new GeneralStaffHandler(clearance);
-                            if (testGeneralStaff.GeneralStaffCheck(clearance) == true)
-                            {
-                                approved = true;
-                                System.out.println("Passing to next section.");
-                                System.out.println("");
-                                System.out.println("");
-                                System.out.println("");
-                            }
-                            else
-                            {
-                                System.out.println("You do Not have the clearance for this.");
-                                approved = false;
-                            }       break;
-                        }
-                    case "CLEANER":
-                        {
-                            CleanerHandler testCleaner = new CleanerHandler(clearance);
-                            if (testCleaner.CleanerCheck(clearance) == true)
-                            {
-                                approved = true;
-                                System.out.println("Passing to next section.");
-                                System.out.println("");
-                                System.out.println("");
-                                System.out.println("");
-                            }
-                            else
-                            {
-                                System.out.println("You do Not have the clearance for this.");
-                                approved = false;
-                            }       break;
-                        }
-                    case "BOOKING STAFF":
-                        {
-                            BookingStaffHandler testBookingStaff = new BookingStaffHandler(clearance);
-                            if (testBookingStaff.BookingStaffCheck(clearance) == true)
-                            {
-                                approved = true;
-                                System.out.println("Passing to next section.");
-                                System.out.println("");
-                                System.out.println("");
-                                System.out.println("");
-                            }
-                            else
-                            {
-                                System.out.println("You do Not have the clearance for this.");
-                                approved = false;
-                            }       break;
-                        }
-                    case "FLIGHT ATTENDANT":
-                        {
-                            FlightAttendantHandler testFlightAttendant = new FlightAttendantHandler(clearance);
-                            if (testFlightAttendant.FlightAttendantCheck(clearance) == true)
-                            {
-                                approved = true;
-                                System.out.println("Passing to next section.");
-                                System.out.println("");
-                                System.out.println("");
-                                System.out.println("");
-                            }
-                            else
-                            {
-                                System.out.println("You do Not have the clearance for this.");
-                                approved = false;
-                            }       break;
-                        }
-                    case "PILOT":
-                        {
-                            PilotHandler testPilot = new PilotHandler(clearance);
-                            if (testPilot.PilotCheck(clearance) == true)
-                            {
-                                approved = true;
-                                System.out.println("Passing to next section.");
-                                System.out.println("");
-                                System.out.println("");
-                                System.out.println("");
-                            }
-                            else
-                            {
-                                System.out.println("You do Not have the clearance for this.");
-                                approved = false;
-                            }       break;
-                        }
-                    case "FLIGHT CONTROL":
-                        {
-                            FlightControlHandler testFlightControl = new FlightControlHandler(clearance);
-                            if (testFlightControl.FlightControlCheck(clearance) == true)
-                            {
-                                approved = true;
-                                System.out.println("Passing to next section.");
-                                System.out.println("");
-                                System.out.println("");
-                                System.out.println("");
-                            }
-                            else
-                            {
-                                System.out.println("You do Not have the clearance for this.");
-                                approved = false;
-                            }       break;
-                        }
-                    case "MANAGMENT":
-                        {
-                            ManagmentHandler testManagment = new ManagmentHandler(clearance);
-                            if (testManagment.ManagmentCheck(clearance) == true)
-                            {
-                                approved = true;
-                                System.out.println("Passing to next section.");
-                                System.out.println("");
-                                System.out.println("");
-                                System.out.println("");
-                            }
-                            else
-                            {
-                                System.out.println("You do Not have the clearance for this.");
-                                approved = false;
-                            }       break;
-                        }
-                    default:
-                        System.out.println("Error - not an option, the program will now terminate.");
-                        approved = true;
-                        break;
-                }
-            }
-            approved = false;
-        }
-        }
-        else
-        {
-            System.out.println("Error - No such clearance. The Program mwill now terminate.");
-        }
+        test.ClearanceCheck(clearance);
+        
+        
+        /**
+         * 
+         */
+        Chainable G = new GeneralStaffHandler(clearance);
+        
+        Chainable C = new GeneralStaffHandler(clearance);
+        
+        Chainable B = new GeneralStaffHandler(clearance);
+        
+        Chainable FA = new GeneralStaffHandler(clearance);
+        
+        Chainable P = new GeneralStaffHandler(clearance);
+        
+        Chainable FC = new GeneralStaffHandler(clearance);
+        
+        Chainable M = new GeneralStaffHandler(clearance);
+        
     }
 }
